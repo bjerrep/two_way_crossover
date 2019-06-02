c3272be3685231973461738ac961114815a7fd15
bjerrep <bjerre3@gmail.com>
2019-06-02 23:23:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# Two Way Crossover
+
+A minimalist project about using a computer and a sound card to quickly produce a two way crossover filter for an analog audio signal chain. It is intended to be used for anything from quick tests and experiments to be installed on e.g. a Raspberry Pi Zero and get permanently embedded in a diy speaker setup.
+
+<p align="center"><img width=400 src="images/response.png"></p>
+
+This projects primary goal is simplicity of use. There are a lot of super nice projects that does the same but they all seem to delivery more features (and design quality) at the expense of added complexity in getting things up and running. And probably a fair guess is that a lot of potential users for a project like this have little or limited experience with linux. Unfortunately even this project still requires at least a little experience with audio on a linux machine.
+
+This is not a HiFi project. It happily decreases the signal to noise ratio by letting the signal pass a couple of 16 bit converters in a sound card most likely chosen from its price tag, just to be filtered by filters with allowed ripple in the pass bands. In other words it probably looks a lot like a lot of consumer audio does these days and chances are that it will do just fine for a lot of diy audio projects.
+
+Implementing the design requirements in software rather than hardware means that just about everything is suddenly infinitely tweakable. Currently the default behavior is tailored towards small and size constrained closed enclosures and it incorporates a bass lift intended to work below the natural low corner frequency to get 'an extra octave' in the low end. (*)
+
+(*) This of course come at a cost. A relatively lot of power might be delivered in the woofer which is basically asked to play in a range where it by nature can't really produce sound. So at least one effect of this is that the max power rating and max cone excursion gets exhausted at a lower apparent sound level than without the boost. So it is a solution for quality rather than quantity. Typically a compensation filter like this for a closed box would be a 6 dB/oct filter but right now its just a couple of bands from a 10 band equalizer.
+
+##Get started
+
+1 Fire up a linux computer with a sound card
+2 Install alsa, gstreamer and python 3 if they are not already available
+3 Clone the software
+4 Run the python script
+
+You now have a single channel digital-analog crossover filter thanks to gstreamer. Default crossover at 2kHz (8'th order 0.25dB ripple Chebyshew) and a 6 dB boost at 59 Hz. Bass in left channel and treble in right channel. A stereo filter will be easy to add in the code and will require an extra sound card since 4 outputs will be needed in total.
+
+##Get started notes
+
+1 Only Arch Linux have been tested but there is no reason to believe that other distroes will misbehave. If using a USB sound card then get one with a real line-in input, assuming that the setup should be based on line signals in the first place. The average headset dongles have mic-in and can be used for testing but most doesn't really belong in a setup like this.
+2 If using a Pi Zero then a network connection is -very- nice while setting up the filter.
+Some example fire and forget install commands are pending..
+3 Make a git clone
+4 Execute
+
+    $python two_way_crossover.py
+    
+Woofer signal will be in left output, tweeter in right output. The script uses the default alsa devices ('hw:0'), use 'aplay -l' to verify if e.g. a USB soundcard is device 1 or larger and modify the script accordingly.
+
+
+#Get tweaking
+
+The central adjustable parameters are found in the configuration file two_way_crossover.json. It is automatically reloaded and applied when modified, there is no reason to restart the python script. If working on a remote computer then start a second ssh for playing with the parameter file. Feel free to use the issue tracker for rants and questions and pull requests for fixes and any nice stuff.
+
+
+# Links
+
+[Pulseaudio Crossover Rack](https://t-5.eu/hp/Software/Pulseaudio%20Crossover%20Rack)
+
---
+++ two_way_crossover.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+import threading
+import time
+import gi
+import os
+import json
+gi.require_version('Gst', '1.0')
+from gi.repository import GLib, Gst
+
+Gst.init(None)
+current_parameters = {}
+
+
+def construct_pipeline():
+    """
+    Launch the gstreamer pipeline for a mono two way crossover.
+    For a bass lift only the low 3 bands of the standard 10 band equalizer are exposed here.
+    This should probably have been a 6 dB/oct shelving filter or something equally exotic
+    but that will be another day.
+    Also note that there are a total of 3 volume controls to aid when playing around, just
+    remove them if they are not needed.
+    The alsa devices 'hw:0' are the default sound card, adjust them to the correct sound card
+    if needed (run 'aplay -l' to see available sound cards).
+    """
+    input = ('alsasrc device=hw:2 ! audioconvert ! audio/x-raw,format=F32LE,channels=2 ! '
+             'queue ! deinterleave name=d ')
+
+    output = ('interleave name=i0 ! capssetter caps = audio/x-raw,channels=2,channel-mask=0x3 ! '
+              'audioconvert ! audioresample ! queue ! '
+              'volume name=master_vol volume=0.001 ! '
+              'alsasink device=hw:2 sync=true buffer-time=100 ')
+
+    # pick which of the two input channels to play
+    tee = 'd.src_0 ! tee name=t0 '
+
+    low = ('t0.src_0 ! queue ! '
+           'equalizer-10bands name=equalizer band0=0.0 band1=6.0 band2=0.0 ! ' 
+           'audiocheblimit name=low_xover poles=8 mode=low-pass cutoff=2000.0 ! '
+           'volume name=low_vol volume=1.0 ! i0.sink_0 ')
+
+    high = ('t0.src_1 ! queue ! audiocheblimit name=high_xover poles=8 mode=high-pass cutoff=2000.0 ! '
+            'volume name=high_vol volume=1.0 ! i0.sink_1 ')
+
+    launch = input + output + tee + low + high
+
+    pipeline = Gst.parse_launch(launch)
+
+    pipeline.set_state(Gst.State.PLAYING)
+
+    return pipeline
+
+
+def reload(parameters):
+    """
+    There is no sanity checking so you get what you ask for. Lookup the documentation
+    for the gstreamer elements if in doubt about what sane values could look like.
+    Keep master volume above 0.001 or the pipeline will stop (!?).
+    """
+    global current_parameters
+
+    print('reloading parameters')
+    modified = dict(parameters.items() - current_parameters.items())
+    for key, value in sorted(modified.items()):
+        print(f' - setting {key} to {value}')
+
+        # Common output volume
+        if key == 'volume':
+            gst_pipeline.get_by_name('master_vol').set_property('volume', value)
+
+        # Bass crossover, equalizer and volume.
+        elif key == 'low_frequency':
+            gst_pipeline.get_by_name('low_xover').set_property('cutoff', value)
+        elif key == 'low_order':
+            gst_pipeline.get_by_name('low_xover').set_property('poles', value)
+        elif key == 'low_volume':
+            gst_pipeline.get_by_name('low_vol').set_property('volume', value)
+        elif key == 'low_eq_29Hz':
+            gst_pipeline.get_by_name('equalizer').set_property('band0', value)
+        elif key == 'low_eq_59Hz':
+            gst_pipeline.get_by_name('equalizer').set_property('band1', value)
+        elif key == 'low_eq_119Hz':
+            gst_pipeline.get_by_name('equalizer').set_property('band2', value)
+
+        # Treble crossover and volume
+        elif key == 'high_frequency':
+            gst_pipeline.get_by_name('high_xover').set_property('cutoff', value)
+        elif key == 'high_order':
+            gst_pipeline.get_by_name('high_xover').set_property('poles', value)
+        elif key == 'high_volume':
+            gst_pipeline.get_by_name('high_vol').set_property('volume', value)
+
+        else:
+            print(f' - error, unknown key {key}')
+
+    current_parameters = parameters
+
+
+def parameter_file_watcher():
+    """
+    Dynamically reloads the parameter file whenever its modified time changes
+    """
+    parameter_file = 'two_way_crossover.json'
+    mtime = None
+    while True:
+        _ = os.path.getmtime(parameter_file)
+        if _ != mtime:
+            mtime = _
+            with open(parameter_file) as f:
+                reload(json.loads(f.read()))
+        time.sleep(1)
+
+
+gst_pipeline = construct_pipeline()
+
+parameter_file_watcher_thread = threading.Thread(target=parameter_file_watcher)
+parameter_file_watcher_thread.run()
+
+GLib.MainLoop().run()
